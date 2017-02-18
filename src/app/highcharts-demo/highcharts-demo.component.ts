e50191cc5bd054407e26a49c2e7452898527507e
zhongzhong0505 <zt_zhong@qq.com>
2017-02-18 20:29:39 +0800

    提交一点代码

--- src/app/highcharts-demo/highcharts-demo.component.ts
+++ src/app/highcharts-demo/highcharts-demo.component.ts
@@ -57,7 +57,7 @@ export class HighchartsDemoComponent implements OnInit {
     series: [{
       data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
     }]
-  }
+  };
 
   chartOptions3 = {
     chart: {
@@ -102,7 +102,7 @@ export class HighchartsDemoComponent implements OnInit {
         ['Others', 0.7]
       ]
     }]
-  }
+  };
   constructor() { }
 
   ngOnInit() {
